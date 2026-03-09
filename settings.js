b4b69d31b83af2c265631d059c2243f68a524cf7
badboy07521253 <rajabrashid2017@gmail.com>
2026-03-09 17:21:10 +0300

    Update owner phone number in settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
+global.ownernomer = "255629157621"
 global.ownername = "🐛 MR WASI "
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
