54bbb8933babdd1b05ddf21e9167fa0baf00751b
badboy07521253 <rajabrashid2017@gmail.com>
2026-03-09 16:54:54 +0300

    Update owner contact details in settings.js

--- settings.js
+++ settings.js
@@ -3,14 +3,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923192173398"
-global.ownername = "🐛 MR WASI "
+global.ownernomer = "255629157621"
+global.ownername = "Badboy"
 global.ytname = "YT: Dont have"
 global.socialm = "GitHub: private"
 global.location = "Pakistan, Lahore"
 
-global.ownernumber = '923347748038'  //creator number
-global.ownername = ' MR WASI ' //owner name
+global.ownernumber = '255629157621'  //creator number
+global.ownername = ' Badboy ' //owner name
 global.botname = 'WASI TECH BUG BOT ' //name of the bot
 
 //sticker details
